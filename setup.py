1cdd0ba149a238d331093ef7a75877a4aa22b062
patlewig <patlewig@users.noreply.github.com>
2022-06-03 17:50:14 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis to support the manuscript: 'Towards reproducible structure-based chemical categories for PFAS to inform and evaluate toxicity and toxicokinetic testing'.',
+    author='patlewig',
+    license='MIT',
+)
